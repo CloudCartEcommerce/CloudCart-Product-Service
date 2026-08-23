7c558a8cee8efb8a25af55fd739149bb3a49261d
Arjun Nigam <arjunnigam.1713@gmail.com>
2026-08-23 14:38:02 +0530

    Added Product Service CI Pipeline

--- src/test/java/com/cloudcart/product/ProductServiceApplicationTests.java
+++ src/test/java/com/cloudcart/product/ProductServiceApplicationTests.java
@@ -3,7 +3,6 @@ package com.cloudcart.product;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
